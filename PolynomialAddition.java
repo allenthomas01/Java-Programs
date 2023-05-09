b0d44700483e7e801096bba09d9e543f6ba27715
Allen <allenthomas204@gmail.com>
2023-05-09 23:46:36 +0530

    Add polynomial addition

---
+++ PolynomialAddition.java
@@ -0,0 +1,28 @@
+/**
+ * @ Author: Allen
+ * @ Create Time: 2023-05-09 22:57:28
+ * @ Modified time: 2023-05-09 23:43:55
+ * @ Description:
+ */
+
+
+import java.util.Scanner;
+
+class Polynomial{
+    int[] a,b,c;
+    int term1;
+    Scanner sc = new Scanner(System.in);
+    Polynomial(){
+        System.out.println("enter total terms");
+        this.term1 = sc.nextInt();
+    }
+
+}
+
+public class PolynomialAddition {
+    public static void main(String[] args){
+        Polynomial obj1= new Polynomial();
+        
+    }
+    
+}
