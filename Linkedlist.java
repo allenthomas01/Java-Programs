36cb217c64804086db66de2385d1206954cdd5c1
Allen <allenthomas204@gmail.com>
2023-05-15 00:02:52 +0530

    Add linkedlist

---
+++ Linkedlist.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Linkedlist{
+    public static void main(String[] args){
+        LinkedList<String> ll = new LinkedList<String>();
+        ll.add("a");
+        ll.add("b");
+        ll.add("c");
+        System.out.println(ll);
+    }
+}
