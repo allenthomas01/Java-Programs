1d2c36c4d65ecee735819f864c7ec7f7d056d056
Allen <allenthomas204@gmail.com>
2023-05-15 20:30:33 +0530

    Update Linkedlist.java

--- Linkedlist.java
+++ Linkedlist.java
@@ -2,9 +2,18 @@ import java.util.*;
 class Linkedlist{
     public static void main(String[] args){
         LinkedList<String> ll = new LinkedList<String>();
-        ll.add("a");
+        ll.add("a"); //add element
         ll.add("b");
         ll.add("c");
         System.out.println(ll);
+        ll.remove(2); //remove element
+        ll.addFirst("f");  // add element to first
+        System.out.println(ll);
+        ll.add("t");
+        ll.add("r");
+        ll.get(3); //element at 3 index
+        ll.addLast("z"); // add element to last
+        System.out.println(ll);
+
     }
 }
